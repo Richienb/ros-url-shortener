dd3b6321a76bc63a6ca066e477746e5d08a0e112
System <none>
2017-06-19 18:39:08 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -12,17 +12,51 @@ app.get("/", function (request, response) {
 
 app.get("/list", function (request, response) {
   client.connect(dbUrl, function (err, db) {
-    if (err) {
-      console.log(err);
-      response.send(500);
-    } else {
-      db.collection('urls').find({}).toArray(function(err, docs){
+    if (err) serverError(err, response)
+    
+    db.collection('urls').find({}).toArray(function(err, docs){
+      if (err) serverError(err, response)
+      
       db.close();
       response.status(200).send(docs);
-      });
-    }
+    });
+    
   });
 });
+
+app.get("/new/:data", function (request, response) {
+  client.connect(dbUrl, function (err, db) {
+    if (err) serverError(err, response)
+    
+    db.collection('urls').find({}).toArray(function(err, docs){
+      if (err) serverError(err, response)
+      
+      db.close();
+      response.status(200).send(docs);
+    });
+    
+  });
+});
+
+app.get("/:data", function (request, response) {
+  client.connect(dbUrl, function (err, db) {
+    if (err) serverError(err, response)
+    
+    if isNumber()
+    db.collection('urls').find({ id: request.params.data }).toArray(function(err, docs){
+      if (err) serverError(err, response)
+      
+      db.close();
+      response.status(200).send(docs);
+    });
+    
+  });
+});
+
+function serverError(err, response){
+  console.error(err);
+  response.send(500);
+}
 
 
 // listen for requests :)
