667e24109c708a80b13f6a817e321cf37fc7d95b
System <none>
2017-06-19 18:50:08 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -28,12 +28,10 @@ app.get("/new/:data", function (request, response) {
   client.connect(dbUrl, function (err, db) {
     if (err) serverError(err, response)
     
-    db.collection('urls').find({}).toArray(function(err, docs){
-      if (err) serverError(err, response)
-      
-      db.close();
-      response.status(200).send(docs);
-    });
+    //Insert new document
+    //Check if it already exists: return the existing one
+    //Autoincrement?
+    
     
   });
 });
@@ -41,13 +39,12 @@ app.get("/new/:data", function (request, response) {
 app.get("/:data", function (request, response) {
   client.connect(dbUrl, function (err, db) {
     if (err) serverError(err, response)
+    if (isNaN(request.params.data)) { response.status(409).send('Invalid URL'); }
     
-    if isNumber()
-    db.collection('urls').find({ id: request.params.data }).toArray(function(err, docs){
+    db.collection('urls').find({ id: parseInt(request.params.data) }).toArray(function(err, docs){
       if (err) serverError(err, response)
-      
       db.close();
-      response.status(200).send(docs);
+      response.redirect(docs[0].original_url);
     });
     
   });
@@ -57,7 +54,6 @@ function serverError(err, response){
   console.error(err);
   response.send(500);
 }
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
