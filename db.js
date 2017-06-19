c5511242eb10a013fe55ab115705a89c74e14c03
System <none>
2017-06-19 17:04:14 +0000

    Checkpoint

--- db.js
+++ db.js
@@ -1,16 +1,19 @@
 var mongodb = require('mongodb');
 var dbUrl = process.env.MONGOLAB_URI;
 var MongoClient = mongodb.MongoClient;
-var module.exports = exports = {}
+module.exports = exports = {}
 
-MongoClient.connect(dbUrl, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', dbUrl);
-    
-    //collection called urls;
-    
-    db.close();
-  }
-});
+
+exports.list = function(){
+  MongoClient.connect(dbUrl, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      var urls = db.collection('urls');
+      var arr = urls.find({}).toArray(function(err, docs){
+        db.close();
+        return docs;  
+      });
+    }
+  });  
+}
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
-
+var db = require('./db.js');
 
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+app.get("/list", function (request, response) {
+  response.send(db.list());
+});
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
