7c2e0af53541460f4fd59ba3f2fa42b7baf6e2c4
System <none>
2017-06-19 13:59:04 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var app = express();
 
+var mongodb = require('mongodb');
+var dbUrl = process.env.MONGOLAB_URI;
+var MongoClient = mongodb.MongoClient;
+
+MongoClient.connect(dbUrl, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', dbUrl);
+
+    db.close();
+  }
+});
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
