9e677727b2800eaa668f5199a403dd59d7967992
System <none>
2017-06-20 00:33:25 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var client = mongodb.MongoClient;
 var base_url = 'https://shortener.glitch.com/'
 var validUrl = require('valid-url');
 
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
@@ -21,33 +19,38 @@ app.get("/new/*", function (request, response) {
       } else {
         //If it already exists reuse the information
         db.collection('urls').find({ original_url: request.params[0]}).toArray(function(err, data){
-          if (data.length > 0){
-            return response.status(200).send({ original_url: data[0].original_url , short_url: base_url + data[0].value });
-          } else {
-            //Store
-            var nextVal = 0;
-            db.collection('urls').find().sort({ value: -1}).limit(1).toArray(function(err, max){
-              if (!err){
-                if (max.length > 0){
-                  nextVal = max[0].value + 1;  
-                }
-                db.collection('urls').insert({ value: nextVal, original_url: request.params[0] }, function(err, data) {
-                  if (!err){
-                    response.status(200).send({ original_url: data.ops[0].original_url , short_url: base_url + data.ops[0].value });
-                  } else {
-                    server
+          if (!err){
+            if (data.length > 0){
+              response.status(200).send({ original_url: data[0].original_url , short_url: base_url + data[0].value });
+            } else {
+              //Store
+              var nextVal = 0;
+              db.collection('urls').find().sort({ value: -1}).limit(1).toArray(function(err, max){
+                if (!err){
+                  if (max.length > 0){
+                    nextVal = max[0].value + 1;  
                   }
-                });                  
-              }
-            });            
-          }
+                  db.collection('urls').insert({ value: nextVal, original_url: request.params[0] }, function(err, data) {
+                    if (!err){
+                      response.status(200).send({ original_url: data.ops[0].original_url , short_url: base_url + data.ops[0].value });
+                    } else {
+                      serverError(err, response);
+                    }
+                  });                  
+                } else {
+                  serverError(err, response);
+                }
+              });            
+            }            
+          } else {
+            serverError(err, response);
+          } 
         });
-
       }
     } else {
       serverError(err, response);
     }
-    db.close();
+    
   });
 });
 
@@ -74,7 +77,7 @@ app.get("/:data(\\d+)/", function (request, response) {
 
 function serverError(err, response){
   console.error(err);
-  response.send(500);
+  response.sendStatus(500);
 }
 
 // listen for requests :)
