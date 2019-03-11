d31677c86b7903f60629abe1371e72772a04ea9e
Glitch (best-position) <none>
2019-03-11 04:20:25 +0000

    💈🍪 Checkpoint ./server.js:1729229/4106

--- server.js
+++ server.js
@@ -1,67 +1,34 @@
 const express = require("express")
 const app = express()
+const path = require("path")
+const https = require("https")
 const endpoint = process.env.ENDPOINT
 
-
-
-const getRandomString = ()  => {
-    let text = ""
-    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length))
-    return text
-}
-
-const getLongUrl = (url) => {
-     var protocol_ok = url.startsWith("http://") || url.startsWith("https://") || url.startsWith("ftp://");
-     if(!protocol_ok){
-         newurl = "http://"+url;
-         return newurl;
-     }else{
-         return url;
-     }
-  
-  const genHash = () => {
-    if (window.location.hash == ""){
-        window.location.hash = getRandomString();
-    }
-}
-  
-  function shortUrl(){
-    var longurl = getLongUrl();
-    genHash();
-    send_request(longurl);
-}
-
 app.get("/", (_request, response) => {
-    response.sendFile("index.html");
+    response.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Match creation request
 app.get("/new/*", (request, response) => {
- 
+
 });
 
-app.get("/:data(\\d+)/", function(request, response) {
-    client.connect(dbUrl, function(err, db) {
-        if (!err) {
-            db.collection('urls').find({
-                value: parseInt(request.params.data)
-            }).toArray(function(err, docs) {
-                if (!err) {
-                    if (docs.length > 0) {
-                        response.redirect(docs[0].original_url);
-                    } else {
-                        response.sendStatus(404);
-                    }
-                } else {
-                    serverError(err, response);
-                }
-                db.close();
-            });
-        } else {
-            serverError(err, response);
-            db.close();
-        }
+// Match lookup request
+app.get("/[0-9]+", (request, response) => {
+    https.get({
+        host: endpoint,
+        port: 443,
+        path: '/'
+    }, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+
+    }).on('error', (e) => {
+        console.error(e);
     });
 });
 
